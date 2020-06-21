5008765d463131ebba3e97f54e72687511b85986
Konrad Gumny <30446684+SadOnion@users.noreply.github.com>
2020-06-21 19:14:40 +0200

    Init

---
+++ ProjectFiles/Data/ProjectModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Data
+{
+    public class ProjectModel
+    {
+        public string Icon {get; set;}
+        public string[] Tags {get; set;}
+        public string Description {get; set;}
+
+        public string GitHubLink {get; set;}
+    }
+}
